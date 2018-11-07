14b23996f26463063e1067aa968ccf3f1ff90c32
pabrodez <pabrodez@gmail.com>
2018-11-07 13:42:09 +0000

    Update script.R

--- script.R
+++ script.R
@@ -57,19 +57,29 @@ df_srft$date_15 <- lubridate::dmy(df_srft$date_15)
 df_srft$surgery_duration <- lubridate::hms(df_srft[!grep("$hour", df_srft$surgery_duration, values = FALSE, ignore.case = TRUE), ]$surgery_duration)
 # days
 df_srft$hospital_stay <- as.numeric(df_srft$hospital_stay)
+
+# strange dates and times
+df_srft[lubridate::year(df_srft$admission_date) < 2017, ]$admission_date <- NA
+df_srft[lubridate::year(df_srft$surgery_date) < 2017, ]$surgery_date <- NA
+df_srft[lubridate::year(df_srft$discharge_date) < 2017, ]$discharge_date <- NA
+df_srft[lubridate::year(df_srft$date_15) < 2017, ]$date_15 <- NA
+df_srft[lubridate::year(df_srft$hospital_stay) < 0, ]$hospital_stay <- NA
+
 # biomarkers
+df_srft[df_srft$hb %in% c("not measured", "not done", "not recorded"), ]$hb <- NA
 df_srft$hb <- as.numeric(df_srft$hb)
+
+df_srft[df_srft$creatinine %in% c("not measured", "not done", "not recorded"), ]$creatinine <- NA
 df_srft$creatinine <- as.numeric(df_srft$creatinine)
+
+df_srft[df_srft$albumin %in% c("not measured", "not done", "not recorded"), ]$albumin <- NA
 df_srft$albumin <- as.numeric(df_srft$albumin)
-df_srft$albumin <- as.numeric(df_srft$albumin)
+
+df_srft[df_srft$creatinine_base %in% c("not measured", "not done", "not recorded"), ]$creatinine_base <- NA
 df_srft$creatinine_base <- as.numeric(df_srft$creatinine_base)
+
+df_srft[df_srft$creatinine_high %in% c("not measured", "not done", "not recorded"), ]$creatinine_high <- NA
 df_srft$creatinine_high <- as.numeric(df_srft$creatinine_high)
-
-# some dates 
-
-
-
-
 
 
 
