1799c5316c6198743d69de9817b90e84a194e4f2
pabrodez <pabrodrod1@alum.us.es>
2018-11-09 01:16:23 +0100

    Update script.R

--- script.R
+++ script.R
@@ -1,39 +1,25 @@
-# load libraries
-library(readxl)
+library(readr)
 library(tidyverse)
 library(lubridate)
-library(data.table)
-library(devtools)
-library(makeR)
-library(chron)
-library(lattice)
-library(grid)
-library(gridExtra)
-library(ggthemes)
-library(viridis)
+library(stringr)
 
 # Create destination folders
 if (!dir.exists("./data")) dir.create("./data")
 if (!dir.exists("./plots")) dir.create("./plots")
-# Anonymised spreadsheet
-# Name file as eras_srft.xlsx and move manually to /data
- 
-data_path <- list.files("./data", pattern = "eras_srft.xlsx", full.names = TRUE, ignore.case = TRUE)[[1]]
-df_srft <-  read_excel(data_path, sheet = 6, range = cell_cols("E1:BR300")),
-                        col_types = TRUE, col_types = "text", trim_ws = TRUE)
 
+# Name file as eras_srft and move manually to /data
+na_list <- c(
+  0, "0", "", "NA", "na", "n/a", "not known/not recorded",
+  "not measured", "Not Measured", "none measured", "None Measured", "not done", "Not Done",
+  "n/d", "Not Recorded", "not recorded", "u/k", "unknown"
+)
+
+data_path <- list.files("./data", pattern = "eras_srft", full.names = TRUE, ignore.case = TRUE)[[1]]
+df_srft <- read_csv(data_path, trim_ws = TRUE, na = na_list, n_max = 300, col_types = cols(.default = "c"))
 # lower caps
 df_srft[] <- lapply(df_srft, tolower)
-
-# Missing values
-df_srft[df_srft == ""] <- NA
-df_srft[df_srft == "n/a"] <- NA
-df_srft[df_srft == "na"] <- NA
-df_srft[df_srft == "not known/not recorded"] <- NA
-df_srft[df_srft == "not done"] <- NA
-df_srft[df_srft == "not recorded"] <- NA
-df_srft[df_srft == "u/k"] <- NA
-df_srft[df_srft == "unkown"] <- NA
+# n/d is not removed in na argument. Try this maybe:
+df_srft[] <- lapply(df_srft, function(x) ifelse(x %in% na_list, NA, x))
 
 # Remove rows with empty values in all columns
 rem_row <- function(x) {
@@ -50,60 +36,88 @@ df_srft <- rem_col(df_srft)
 
 # Set fields types
 # dates and times
-df_srft$admission_date <- lubridate::dmy(df_srft$admission_date)
-df_srft$surgery_date <- lubridate::dmy(df_srft$surgery_date)
-df_srft$discharge_date <- lubridate::dmy(df_srft$discharge_date)
-df_srft$date_15 <- lubridate::dmy(df_srft$date_15)
-df_srft$surgery_duration <- lubridate::hms(df_srft[!grep("$hour", df_srft$surgery_duration, values = FALSE, ignore.case = TRUE), ]$surgery_duration)
+# df_srft$discharge_date <- as.numeric(df_srft$discharge_date)
+# df_srft$discharge_date <- as.Date(df_srft$discharge_date, origin = "1899-12-30")
+df_srft$discharge_date <- as.Date(df_srft$discharge_date, format = "%m/%d/%Y")
+
+# df_srft$admission_date <- as.numeric(df_srft$admission_date)
+# df_srft$admission_date <- as.Date(df_srft$admission_date, origin = "1899-12-30")
+df_srft$admission_date <- as.Date(df_srft$admission_date, format = "%m/%d/%Y")
+
+# df_srft$surgery_date <- as.numeric(df_srft$surgery_date)
+# df_srft$surgery_date <- as.Date(df_srft$surgery_date, origin = "1899-12-30")
+df_srft$surgery_date <- as.Date(df_srft$surgery_date, format = "%m/%d/%Y")
+
+# df_srft$date_15 <- as.numeric(df_srft$date_15)
+# df_srft[df_srft$date_15 == 0, ]$date_15 <- NA
+df_srft$date_15 <- as.Date(df_srft$date_15, format = "%m/%d/%Y")
+
+df_srft$date_school <- as.Date(df_srft$date_school, format = "%m/%d/%Y")
+
+# df_srft[df_srft$surgery_duration == 0, ]$surgery_duration <- NA
+df_srft[grep("hour[s]$", df_srft$surgery_duration), ]$surgery_duration <- NA
+df_srft$surgery_duration <- lubridate::hms(df_srft$surgery_duration)
+df_srft$surgery_duration <- lubridate::time_length(df_srft$surgery_duration, unit = "minutes")
+
 # days
 df_srft$hospital_stay <- as.numeric(df_srft$hospital_stay)
+df_srft$hospital_stay <- ifelse(df_srft$hospital_stay < 0, NA, df_srft$hospital_stay)
 
 # strange dates and times
-df_srft[lubridate::year(df_srft$admission_date) < 2017, ]$admission_date <- NA
-df_srft[lubridate::year(df_srft$surgery_date) < 2017, ]$surgery_date <- NA
-df_srft[lubridate::year(df_srft$discharge_date) < 2017, ]$discharge_date <- NA
-df_srft[lubridate::year(df_srft$date_15) < 2017, ]$date_15 <- NA
-df_srft[lubridate::year(df_srft$hospital_stay) < 0, ]$hospital_stay <- NA
+# df_srft[lubridate::year(df_srft$admission_date) < 2017, ]$admission_date <- NA
+# df_srft[lubridate::year(df_srft$surgery_date) < 2017, ]$surgery_date <- NA
+# df_srft[lubridate::year(df_srft$discharge_date) < 2017, ]$discharge_date <- NA
+# df_srft[lubridate::year(df_srft$date_15) < 2017, ]$date_15 <- NA
 
 # biomarkers
-df_srft[df_srft$hb %in% c("not measured", "not done", "not recorded"), ]$hb <- NA
+df_srft$hb <- sub(",", ".", df_srft$hb, fixed = TRUE)
 df_srft$hb <- as.numeric(df_srft$hb)
 
-df_srft[df_srft$creatinine %in% c("not measured", "not done", "not recorded"), ]$creatinine <- NA
+df_srft$creatinine <- sub(",", ".", df_srft$creatinine, fixed = TRUE)
 df_srft$creatinine <- as.numeric(df_srft$creatinine)
 
-df_srft[df_srft$albumin %in% c("not measured", "not done", "not recorded"), ]$albumin <- NA
+df_srft$albumin <- sub(",", ".", df_srft$albumin, fixed = TRUE)
 df_srft$albumin <- as.numeric(df_srft$albumin)
 
-df_srft[df_srft$creatinine_base %in% c("not measured", "not done", "not recorded"), ]$creatinine_base <- NA
+df_srft$creatinine_base <- sub(",", ".", df_srft$creatinine_base, fixed = TRUE)
 df_srft$creatinine_base <- as.numeric(df_srft$creatinine_base)
 
-df_srft[df_srft$creatinine_high %in% c("not measured", "not done", "not recorded"), ]$creatinine_high <- NA
+df_srft$creatinine_high <- sub(",", ".", df_srft$creatinine_high, fixed = TRUE)
 df_srft$creatinine_high <- as.numeric(df_srft$creatinine_high)
 
+df_srft$creatinine_high <- sub(",", ".", df_srft$creatinine_high, fixed = TRUE)
+df_srft$creatinine_high <- as.numeric(df_srft$creatinine_high)
 
+df_srft$troponin <- sub(",", ".", df_srft$troponin, fixed = TRUE)
+df_srft$troponin <- sub("<", "-", df_srft$troponin)
+df_srft$troponin <- as.numeric(df_srft$troponin)
 
+df_srft$troponin_7 <- sub(",", ".", df_srft$troponin_7, fixed = TRUE)
+df_srft$troponin_7 <- sub("<", "-", df_srft$troponin_7, fixed = TRUE)
+df_srft$troponin_7 <- as.numeric(df_srft$troponin_7)
 
+# other fields
+df_srft$discharge_loc <- sub("critical care (level 2)", "ccu (level 2)", df_srft$discharge_loc, fixed = TRUE)
+df_srft$discharge_loc <- sub("ccu (level 2 and 3)", "ccu (level 2/3)", df_srft$discharge_loc, fixed = TRUE)
+df_srft$discharge_loc <- str_replace(df_srft$discharge_loc, "^hdu$", "ccu")
 
+df_srft$current_activity <- sapply(strsplit(df_srft$current_activity, ","), `[`, 1)
 
+df_srft$anaesthesia <- sapply(strsplit(df_srft$anaesthesia, ",\\s"), `[`, 1)
 
+df_srft$tidal_vol <- sub(",", ".", df_srft$tidal_vol, fixed = TRUE)
 
+# vector with all anaemia treatments 
+anae_treat_vec <- unlist(strsplit(df_srft$anaemia_treat, ",\\s"))
+# also:
+# df_srft$anaemia_treat2 <- vector(mode = "character", length = nrow(df_srft))
+# df_srft$anaemia_treat2 <- sapply(strsplit(df_srft$anaemia_treat, ",\\s"), `[`, 2)
+# df_srft$anaemia_treat <- sapply(strsplit(df_srft$anaemia_treat, ",\\s"), `[`, 1)
+# 
+# df_srft %>% select("anaemia_treat", "anaemia_treat2") %>% gather()
 
+sapply(df_srft, unique)
 
-# NAs plot to check visually
-plotNa <- function(dataFrame) {
-        tempDf <- as.data.frame(ifelse(is.na(dataFrame), 0, 1))
-        tempDf <- tempDf[, order(colSums(tempDf))]
-        tempData <- expand.grid(list(x = 1:nrow(tempDf), y = colnames(tempDf)))
-        tempData$v <- as.vector(as.matrix(tempDf))
-        tempData <- data.frame(x = unlist(tempData$x), y = unlist(tempData$y), v = unlist(tempData$v))
-        ggplot(tempData) + geom_tile(aes(x=x, y=y, fill=factor(v))) +
-                scale_fill_manual(values=c("white", "black"), name="Missing value\n1=No, 0=Yes") +
-                theme_light() + ylab("") + xlab("Rows of data set") + ggtitle("")
-        
-}
-
-adults_nas <- plotNa(df_srft)
 
 
 
