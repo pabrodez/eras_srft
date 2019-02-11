84b02b598a20a06bccf930464255dc478a69c3b8
pabrodez <pabrodrod1@alum.us.es>
2019-02-11 14:31:39 +0000

    Update haelo_dashboard.R

--- haelo_dashboard.R
+++ haelo_dashboard.R
@@ -15,44 +15,42 @@ library(ggthemes)
 library(reshape2)
 # 1.1 Read data ---------------------------------------------------------------
 data_path <- list.files("./data", pattern = "\\.csv", full.names = TRUE, ignore.case = TRUE)[[1]]  ## save "collated" sheet as a single .csv file and move manually to folder "data"
-df_srft <- read.csv(data_path, strip.white = TRUE, stringsAsFactors = FALSE, nrow = 305)           ## specify number of rows to read as excel tends to generate blank rows, slowing this step      
+df_srft <- read.csv(data_path, strip.white = TRUE, stringsAsFactors = FALSE, nrow = 310)           ## specify number of rows to read as excel tends to generate blank rows, slowing this step      
 df_srft[, sapply(df_srft, is.character)] <- sapply(df_srft[, sapply(df_srft, is.character)],
-                                                   iconv,
-                                                   from = "WINDOWS-1252", to = "UTF-8"
-)
+                                                   iconv, from = "WINDOWS-1252", to = "UTF-8")
 # 1.2 Data cleaning -----------------------------------------------------------
 df_srft[] <- lapply(df_srft, function(x)
-        if (is.character(x) == TRUE) {
-                tolower(x)
-        } else {
-                x
-        })
+  if (is.character(x) == TRUE) {
+    tolower(x)
+  } else {
+    x
+  })
 
 df_srft[] <- lapply(df_srft, function(x)
-        if (is.character(x) == TRUE) {
-                trimws(x)
-        } else {
-                x
-        })
+  if (is.character(x) == TRUE) {
+    trimws(x)
+  } else {
+    x
+  })
 
 # Replace not recorded and missing values with NAs (to be discussed)
 na_list <- c(
-        0, "0", "", "NA", "na", "n/a", "<NA>", "not known/not recorded", "n/r",
-        "not measured", "Not Measured", "none measured", "None Measured", "not done", "Not Done",
-        "n/d", "Not Recorded", "not recorded", "u/k", "unknown", "#ref!"
+  0, "0", "", "NA", "na", "n/a", "<NA>", "not known/not recorded", "n/r",
+  "not measured", "Not Measured", "none measured", "None Measured", "not done", "Not Done",
+  "n/d", "Not Recorded", "not recorded", "u/k", "unknown", "#ref!"
 )
 df_srft[] <- lapply(df_srft, function(x) ifelse(x %in% na_list, NA, x))
 
 # Remove rows with empty values in all columns
 remove_blankr <- function(x) {
-        x[rowSums(is.na(x)) != ncol(x), ]
+  x[rowSums(is.na(x)) != ncol(x), ]
 }
 
 df_srft <- remove_blankr(df_srft)
 
 # Remove columns with empty values in all rows
 remove_blankc <- function(x) {
-        x[, colSums(is.na(x)) != nrow(x)]
+  x[, colSums(is.na(x)) != nrow(x)]
 }
 
 df_srft <- remove_blankc(df_srft)
@@ -69,12 +67,12 @@ df_srft$date_15 <- as.Date(df_srft$date_15, format = "%d/%m/%Y")
 df_srft$date_school <- as.Date(df_srft$date_school, origin = "1899-12-30")
 
 df_srft[] <- lapply(df_srft, function(x) {
-        if (class(x) == "Date") {
-                dplyr::if_else(lubridate::year(x) < 2018, as.Date(NA), x)
-        }
-        else {
-                (x)
-        }
+  if (class(x) == "Date") {
+    dplyr::if_else(lubridate::year(x) < 2018, as.Date(NA), x)
+  }
+  else {
+    (x)
+  }
 })
 
 df_srft$hospital_stay <- as.numeric(df_srft$hospital_stay)
@@ -137,85 +135,79 @@ df_srft$gastro <- ifelse(!is.na(df_srft$gastro) & df_srft$gastro == "experienced
 # 1.3 Transform --------------------------------------------------------------
 df_srft <- rename(df_srft, comment_pre = comment)  # Change col name and remove cancelled and non elegible patients
 df_srft <- df_srft[(!df_srft$comment_pre %in% c("cancelled", "down's syndrome") | is.na(df_srft$comment_pre)) &
-                           (df_srft$gm_eras_ele != "no" | is.na(df_srft$gm_eras_ele)), ]
+                     (df_srft$gm_eras_ele != "no" | is.na(df_srft$gm_eras_ele)), ]
 df_srft$week_start <- cut.Date(df_srft$surgery_date, breaks = "week")  ## week of surgery
 df_srft$month_surgery <- format(as.Date(df_srft$surgery_date), "%Y-%m")  ## month of surgery
 df_srft$discharge_my <- format(as.Date(df_srft$discharge_date), "%Y-%m")  ## month of discharge
 
 # Data frame with relevant columns for haelo dashboard
 df_haelo <-
-        df_srft %>%
-        filter(eras == "yes" | is.na(eras)) %>%   ## only eras patients
-        dplyr::select(
-                inc_spiro, t_brushes, m_washes, oral_diet, mobilised,
-                surgery_school, chest_physio, week_start, month_surgery, area_surgery, hospital_stay, infec_7, pulm_supp_7,
-                readmit_30, surgery_date, discharge_date, discharge_my
-        ) %>%  ## select columns relevant for haelo dashboard
-        mutate(nas_columns = rowSums(is.na(dplyr::select(., 1:5)))) %>%  ## this will allow to filter in iCough measures
-        mutate(inc_spiro = if_else(inc_spiro %in% c(">two", "once", "twice"), 1, 0)) %>%
-        mutate_at(c("t_brushes", "m_washes"), function(x) {
-                if_else(x == "twice", 1, 0)
-        }) %>%
-        mutate_at(c("oral_diet", "mobilised"), function(x) {
-                if_else(x == "yes", 1, 0)
-        }) %>%
-        mutate(
-                compliance_n = rowSums(dplyr::select(., 1:5), na.rm = TRUE),
-                compliant = if_else(compliance_n >= 4, "yes", "no")
-        )
+  df_srft %>%
+  filter(eras == "yes" | is.na(eras)) %>%   ## only eras patients
+  dplyr::select(
+    inc_spiro, t_brushes, m_washes, oral_diet, mobilised,
+    surgery_school, chest_physio, week_start, month_surgery, area_surgery, hospital_stay, infec_7, pulm_supp_7,
+    readmit_30, surgery_date, discharge_date, discharge_my
+  ) %>%  ## select columns relevant for haelo dashboard
+  mutate(nas_columns = rowSums(is.na(dplyr::select(., 1:5)))) %>%  ## this will allow to filter in iCough measures
+  mutate(inc_spiro = if_else(inc_spiro %in% c(">two", "once", "twice"), 1, 0)) %>%
+  mutate_at(c("t_brushes", "m_washes"), function(x) {
+    if_else(x == "twice", 1, 0)
+  }) %>%
+  mutate_at(c("oral_diet", "mobilised"), function(x) {
+    if_else(x == "yes", 1, 0)
+  }) %>%
+  mutate(
+    compliance_n = rowSums(dplyr::select(., 1:5), na.rm = TRUE),
+    compliant = if_else(compliance_n >= 4, "yes", "no")
+  )
 
 # 1.4 Measures ----------------------------------------------------------------
 # 1.4.1 Patients that attended Surgery School
-get_school <- function(data) {
-        data %>%
-                dplyr::select(area_surgery, surgery_school, chest_physio, week_start) %>%
-                # filter(area_surgery == "")  ## can be used here to select a specific area of surgery
-                group_by(week_start) %>%
-                summarise(
-                        n = n(),
-                        school_n = sum(
-                                surgery_school %in% c("yes - hospital session", "yes -  online") |
-                                        chest_physio %in% c("nurse", "doctor", "surgery school")
-                        )
-                ) %>%
-                print.data.frame()
+count_surgery <- function(data, time) {
+  dplyr::select(data, area_surgery, surgery_school, chest_physio, week_start, month_surgery) %>%
+    group_by(time) %>%
+    summarise(
+      n = n(),
+      school_n = sum(
+        surgery_school %in% c("yes - hospital session", "yes -  online") |
+          chest_physio %in% c("nurse", "doctor", "surgery school")
+      )
+    )
 }
 
 # 1.4.2 Number of readmissions within 30 days by month of discharge
-df_haelo %>%
-  dplyr::select(discharge_my, area_surgery, readmit_30) %>%
-  ## filter() area surgery
-  group_by(discharge_my) %>%
-  summarise(
-    n_patients = n(), ## number of pts discharged that month
-    readmit_na = sum(is.na(readmit_30)), ## number of patients for whom 30 after discharge haven't passed
-    number_readmit = sum(readmit_30 == "yes", na.rm = TRUE)
-  ) %>% ## number of readmission
-  print.data.frame()
+count_readm <- function(data, time) {
+  dplyr::select(data, discharge_my, area_surgery, readmit_30, week_start) %>%
+    group_by(time) %>%
+    summarise(
+      n_patients = n(), ## number of pts discharged that month
+      readmit_na = sum(is.na(readmit_30)), ## number of patients for whom 30 days after discharge haven't passed
+      number_readmit = sum(readmit_30 == "yes", na.rm = TRUE)
+    )
+}
 
 # 1.4.3 Number of patients with Postoperative Pulmonary Complication by week of surgery
-df_haelo %>%
-  dplyr::select(week_start, infec_7, pulm_supp_7, area_surgery) %>%
-  ## filter() area surgery
-  group_by(week_start) %>%
-  summarise(
-    n_patients = n(),
-    patients_ppc = sum(infec_7 == "chest" |
-      pulm_supp_7 %in% c("moderate", "mild", "severe"), na.rm = TRUE)
-  ) %>%
-  print.data.frame()
+count_ppc <- function(data, time) {
+  dplyr::select(data, week_start, infec_7, pulm_supp_7, area_surgery, month_surgery) %>%
+    group_by(time) %>%
+    summarise(
+      n_patients = n(),
+      patients_ppc = sum(infec_7 == "chest" |
+        pulm_supp_7 %in% c("moderate", "mild", "severe"), na.rm = TRUE)
+    )
+}
 
 # 1.4.4 Average Length of Stay grouped by month of discharge
-df_haelo %>%
+get_los_mean <- function(data, time) {
   dplyr::select(discharge_my, hospital_stay, area_surgery) %>%
-  ## filter() area surgery
-  group_by(discharge_my) %>%
-  summarise(
-    mean_los = mean(hospital_stay, na.rm = TRUE),
-    n_patients = n()
-  ) %>%
-  print.data.frame()
-
+    group_by(time) %>%
+    summarise(
+      mean_los = mean(hospital_stay, na.rm = TRUE),
+      n_patients = n()
+    )
+}
+  
 # 1.4.5 Satisfaction score
 foo_satis <- data.frame(
   score = sample(1:5, 200, replace = TRUE),
@@ -228,86 +220,96 @@ foo_satis %>%
   print.data.frame()
 
 # 1.4.6 Number of patients compliant with iCough bundle
-df_haelo %>%
-  dplyr::select(area_surgery, week_start, compliance_n, compliant) %>%
-  filter(compliance_n != 5) %>% ## Remove pts without all 5 items recorded. Filter area surgery
-  group_by(week_start) %>%
-  summarise(
-    n_patients = n(),
-    n_compliant = sum(compliant == "yes", na.rm = TRUE)
-  ) %>%
-  print.data.frame()
-
+count_icough <- function(data, time) {
+  dplyr::select(data, area_surgery, week_start, nas_columns, compliant) %>%
+    filter(nas_columns != 5) %>% ## Remove pts without all 5 items recorded
+    group_by(time) %>%
+    summarise(
+      n_patients = n(),
+      n_compliant = sum(compliant == "yes", na.rm = TRUE)
+    )
+}
 # 1.4.7 Number of patients that had their teeth brushed twice
-df_haelo %>%
-  dplyr::select(area_surgery, week_start, t_brushes) %>%
-  ## filter() area surgery
-  group_by(week_start) %>%
-  summarise(
-    n_patients = n(),
-    n_tbrushes = sum(t_brushes, na.rm = TRUE),
-    n_nas = sum(is.na(t_brushes))
-  ) %>% ## number of patients with NA in tbrushes column
-  print.data.frame()
-
+count_tbrush <- function(data, time) {
+  dplyr::select(data, area_surgery, week_start, t_brushes, nas_columns) %>%
+    filter(nas_columns != 5) %>%
+    group_by(time) %>%
+    summarise(
+      n_patients = n(),
+      n_tbrushes = sum(t_brushes, na.rm = TRUE),
+      n_nas = sum(is.na(t_brushes))
+    )
+}
+  
 # 1.4.8 Number of patients mobilised
-df_haelo %>%
-  dplyr::select(area_surgery, week_start, mobilised) %>%
-  ## filter() area surgery
-  group_by(week_start) %>%
-  summarise(
-    n_patients = n(),
-    n_mob = sum(mobilised, na.rm = TRUE),
-    n_nas = sum(is.na(mobilised))
-  ) %>% ## number of patients with NA in tbrushes column
-  print.data.frame()
+count_mob <- function(data, time) {
+  dplyr::select(data, area_surgery, week_start, mobilised, nas_columns) %>%
+    filter(nas_columns != 5) %>%
+    group_by(time) %>%
+    summarise(
+      n_patients = n(),
+      n_mob = sum(mobilised, na.rm = TRUE),
+      n_nas = sum(is.na(mobilised))
+    )
+}
 
 # 1.4.9 Number of patients that used incentive spirometer
-df_haelo %>%
-  dplyr::select(area_surgery, week_start, inc_spiro) %>%
-  ## filter() area surgery
-  group_by(week_start) %>%
-  summarise(
-    n_patients = n(),
-    n_is = sum(inc_spiro, na.rm = TRUE),
-    n_nas = sum(is.na(inc_spiro))
-  ) %>% ## number of patients with NA in tbrushes column
-  print.data.frame()
+count_spiro <- function(data) {
+  dplyr::select(data, area_surgery, week_start, inc_spiro, nas_columns) %>%
+    filter(nas_columns != 5) %>%
+    group_by(week_start) %>%
+    summarise(
+      n_patients = n(),
+      n_is = sum(inc_spiro, na.rm = TRUE),
+      n_nas = sum(is.na(inc_spiro))
+    )
+}
 
 # 1.4.10 Number of patients that used mouth wash twice
-df_haelo %>%
-  dplyr::select(area_surgery, week_start, m_washes) %>%
-  ## filter() area surgery
-  group_by(week_start) %>%
-  summarise(
-    n_patients = n(),
-    n_mwash = sum(m_washes, na.rm = TRUE),
-    n_nas = sum(is.na(m_washes))
-  ) %>% ## number of patients with NA in tbrushes column
-  print.data.frame()
-# 1.4.11 Number of patients who re-started oral diet
-df_haelo %>%
-  dplyr::select(area_surgery, week_start, oral_diet) %>%
-  ## filter() area surgery
-  group_by(week_start) %>%
-  summarise(
-    n_patients = n(),
-    n_diet = sum(oral_diet, na.rm = TRUE),
-    n_nas = sum(is.na(oral_diet))
-  ) %>% ## number of patients with NA in tbrushes column
-  print.data.frame()
+count_mwash <- function(data) {
+  dplyr::select(data, area_surgery, week_start, m_washes, nas_columns) %>%
+    filter(nas_columns != 5) %>%
+    group_by(week_start) %>%
+    summarise(
+      n_patients = n(),
+      n_mwash = sum(m_washes, na.rm = TRUE),
+      n_nas = sum(is.na(m_washes))
+    )
+}
 
+# 1.4.11 Number of patients who re-started oral diet
+count_diet <- function(data) {
+  dplyr::select(data, area_surgery, week_start, oral_diet, nas_columns) %>%
+    filter(nas_columns != 5) %>%
+    group_by(week_start) %>%
+    summarise(
+      n_patients = n(),
+      n_diet = sum(oral_diet, na.rm = TRUE),
+      n_nas = sum(is.na(oral_diet))
+    )
+}
 
 # 1.4.12 Function to conveniently print any of the above by area of surgery
-print_measure <- function(data, measure_fun, area = "all") {
+print_measure <- function(data, measure_fun, area = "all", time = "month_surgery") {
   if (area == "all") {
-    measure_fun(data)
+    measure_fun(data, time)
   } else {
     df <- filter(data, area_surgery == area)
-    measure_fun(df)
+    measure_fun(data = df, time= enquo(time))
   }
 }
 
+print_measure(df_haelo, count_readm, time="week_start")
+
+count_readm <- function(data, time) {
+  dplyr::select(data, discharge_my, area_surgery, readmit_30, week_start) %>%
+    group_by(time) %>%
+    summarise(
+      n_patients = n(), ## number of pts discharged that month
+      readmit_na = sum(is.na(readmit_30)), ## number of patients for whom 30 days after discharge haven't passed
+      number_readmit = sum(readmit_30 == "yes", na.rm = TRUE)
+    )
+}
 # 2 Read data -------------------------------------------------------------
 df_colorectal <- read_xlsx("./data/colorectal_5_years.xlsx", sheet = 2,
                            col_types = c("skip", "skip", "skip", "date", "date", "text", "text", "skip", 
@@ -316,7 +318,7 @@ df_colorectal <- read_xlsx("./data/colorectal_5_years.xlsx", sheet = 2,
 
 # 2.2 Transform --------------------------------------------------------------------
 colnames(df_colorectal) <- c("admission_date", "discharge_date", "surgery_code", "surgery_name", "los",
-  "readm_30", "mort_30", "mort_90", "cc_stay")
+                             "readm_30", "mort_30", "mort_90", "cc_stay")
 
 df_colorectal[] <- lapply(df_colorectal, function(x)
   if (is.character(x)) {
@@ -365,4 +367,3 @@ ggplot(discharge_df) +
   # ) +
   theme_fivethirtyeight() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-  
