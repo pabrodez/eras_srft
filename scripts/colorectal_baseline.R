05847732fe864667bbbfa07aa805bd59860e3409
pabrodez <pabrodrod1@alum.us.es>
2019-03-15 15:12:10 +0000

    Add files via upload

--- scripts/colorectal_baseline.R
+++ scripts/colorectal_baseline.R
@@ -39,7 +39,6 @@ double_surg <- select(df_colorectal, patient_number, admission_date) %>% ## Inde
 }
 
 df_colorectal$patient_number <- with(df_colorectal, match(patient_number, unique(patient_number)))  ## add patient identifier
-df_colorectal <- df_colorectal[, -1]
 
 # 3  Merge df_haelo and df_colorectal --------------------------------------------------------------------
 colorectal_merged <- df_haelo %>%
@@ -61,9 +60,12 @@ colorectal_merged <- df_haelo %>%
 
 colorectal_merged <- colorectal_merged %>% mutate(
   week_adm = as.Date(cut.Date(.$admission_date, "week")),
-  month_adm = factor(format.Date(.$admission_date, "%Y-%m"), 
-                     levels = format.Date(levels(cut.Date(.$admission_date, "month")), "%Y-%m")),
-  month_dis = format.Date(.$discharge_date, "%Y-%m")
+  month_adm = factor(format.Date(.$admission_date, "%Y-%m"),
+    levels = format.Date(levels(cut.Date(.$admission_date, "month")), "%Y-%m")
+  ),
+  month_dis = factor(format.Date(.$discharge_date, "%Y-%m"),
+    levels = format.Date(levels(cut.Date(.$discharge_date, "month")), "%Y-%m")
+  )
 )
 
 # 4 Plot ----------------------------------------------------------------
@@ -73,15 +75,8 @@ colorectal_merged %>% group_by(month_adm) %>%
   {
     ggplot(., aes(x = month_adm, y = n, group = baseline, colour = baseline)) +
       geom_line() +
-      scale_y_continuous(breaks = seq(min(.$n), max(.$n), 3)) +
+      scale_y_continuous(breaks = seq(min(.$n), max(.$n), 4)) +
       scale_x_discrete(breaks = .$month_adm[seq(1, length(.$month_adm), 4)], drop = FALSE) +
-      geom_segment(aes(x = "2018-03", xend = "2018-03", y = 18, yend = 15),
-                   arrow = arrow(length = unit(0.02, "npc"))
-      ) +
-      geom_segment(aes(x = "2018-07", xend = "2018-07", y = 18, yend = 15),
-                   arrow = arrow(length = unit(0.02, "npc"))
-      ) +
-      annotate("text", x = "2018-05", y = 19, label = "Gap") +
       theme_fivethirtyeight() +
       theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
   }
@@ -103,9 +98,9 @@ colorectal_merged %>%
   na.omit() %>% ## omit pts with LoS = NA
   {
     ggplot(.) +
-      geom_line(aes(x = month_dis, y = value, colour = baseline, group = 1)) +
+      geom_line(aes(x = month_dis, y = value, colour = baseline, group = baseline)) +
       scale_x_discrete(
-        breaks = unique(.$month_dis)[seq(1, length(.$month_dis), 1)]
+        breaks = unique(.$month_dis)[seq(1, length(.$month_dis), 3)]
       ) +
       scale_y_continuous(
         limits = c(min(.$value), max(.$value)),
@@ -126,18 +121,18 @@ colorectal_merged %>%
 # Merged: Box plot LoS by month
 colorectal_merged %>%
   select(month_dis, los, baseline) %>%
-  group_by(month_dis) %>%
   na.omit() %>%
   {
     ggplot(.) +
       geom_boxplot(aes(x = month_dis, y = los, colour = baseline)) +
       scale_x_discrete(
-        breaks = unique(.$month_dis)[seq(1, length(.$month_dis), 3)]
+        breaks = unique(.$month_dis)[seq(1, length(.$month_dis), 3)],
+        drop = FALSE
       ) +
       scale_y_continuous(
-        limits = c(0, 96),
         breaks = seq(min(.$los), max(.$los), 3)
-      ) + ## change interactively if want to keep all observations
+      ) + 
+      coord_cartesian(ylim = c(0, 96)) + ## change interactively if want to keep all observations
       theme_fivethirtyeight() +
       theme(axis.text.x = element_text(angle = 45, hjust = 1))
   }
@@ -145,15 +140,21 @@ colorectal_merged %>%
 # Merged: Histogram LoS by month
 colorectal_merged %>%
   select(month_dis, los, baseline) %>%
-  group_by(month_dis) %>%
   na.omit() %>%
   {
     ggplot(.) +
       geom_histogram(aes(x = los), binwidth = 1, colour = "grey") +
       scale_x_continuous(
         limits = c(0, 100),
-        breaks = seq(min(.$los), max(.$los), 3)
+        breaks = seq(min(.$los), max(.$los), 4)
       ) +
       theme_fivethirtyeight() +
       theme(axis.text.x = element_text(angle = 45, hjust = 1))
   }
+
+# Day of week admission and LoS
+colorectal_merged %>%
+  select(admission_date, los, baseline) %>%
+  mutate(adm_wday = lubridate::wday(admission_date, week_start = 1, label = TRUE))
+  
+  
